e4aa6d3c098b52af1829958f1e7f165b4a73269e
Himanshu Gupta <thinktank.himanshu@gmail.com>
2021-02-01 17:34:13 +0530

    till search

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -1,9 +1,124 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import styled from 'styled-components';
 import { GithubContext } from '../context/context';
 import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from './Charts';
+
 const Repos = () => {
-  return <h2>repos component</h2>;
+  const { repos } = useContext(GithubContext);
+
+  const languages = repos.reduce((total, item) => {
+    const { language, stargazers_count } = item;
+    if (!language) return total;
+
+    if (!total[language]) {
+      total[language] = { label: language, value: 1, stars: stargazers_count };
+    } else {
+      total[language] = {
+        ...total[language],
+        value: total[language].value + 1,
+        stars: total[language].stars + stargazers_count,
+      };
+    }
+    return total;
+  }, {});
+
+  const mostUsed = Object.values(languages)
+    .sort((a, b) => {
+      return b.value - a.value;
+    })
+    .slice(0, 5);
+
+  const mostPopular = Object.values(languages)
+    .sort((a, b) => {
+      return b.stars - a.stars;
+    })
+    .slice(0, 5)
+    .map((entry) => {
+      return { ...entry, value: entry.stars };
+    })
+    .filter((entry) => {
+      return entry.value > 0;
+    });
+
+  // stars, forks
+
+  let { stars, forks } = repos.reduce(
+    (total, item) => {
+      const { stargazers_count, name, forks } = item;
+      total.stars[name] = { label: name, value: stargazers_count };
+      total.forks[name] = { label: name, value: forks };
+      return total;
+    },
+    {
+      stars: {},
+      forks: {},
+    }
+  );
+
+  stars = Object.values(stars)
+    .sort((a, b) => {
+      return b.value - a.value;
+    })
+    .slice(0, 5)
+    .filter((entry) => {
+      return entry.value > 0;
+    });
+
+  forks = Object.values(forks)
+    .sort((a, b) => {
+      return b.value - a.value;
+    })
+    .slice(0, 5)
+    .filter((entry) => {
+      return entry.value > 0;
+    });
+
+  // let languages = [];
+  // repos.forEach((entry) => {
+  //   if (entry.language && !languages.includes(entry.language)) {
+  //     languages.push(entry.language);
+  //   }
+  // });
+
+  // let count = 0;
+  // let pairs = [];
+
+  // languages.forEach((language) => {
+  //   repos.forEach((entry) => {
+  //     if (entry.language === language) {
+  //       count++;
+  //     }
+  //   });
+  //   pairs.push({ label: language, value: count.toString() });
+  //   count = 0;
+  // });
+
+  // console.log(pairs);
+  const chartData = [
+    {
+      label: 'Javascript',
+      value: '90',
+    },
+    {
+      label: 'Python',
+      value: '30',
+    },
+    {
+      label: 'C#',
+      value: '95',
+    },
+  ];
+  return (
+    <section className="section">
+      <Wrapper className="section-center">
+        {/* <ExampleChart data={chartData} /> */}
+        <Pie3D data={mostUsed} />
+        <Column3D data={stars} />
+        <Doughnut2D data={mostPopular} />
+        <Bar3D data={forks} />
+      </Wrapper>
+    </section>
+  );
 };
 
 const Wrapper = styled.div`
--- src/context/context.js
+++ src/context/context.js
@@ -13,8 +13,90 @@ const GithubProvider = ({ children }) => {
   const [repos, setRepos] = useState(mockRepos);
   const [followers, setFollowers] = useState(mockFollowers);
 
+  const [requests, setRequests] = useState(0);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const [error, setError] = useState({ show: false, msg: '' });
+
+  // check rate
+  const checkRequests = async () => {
+    try {
+      // directly in json
+      const response = await axios(`${rootUrl}/rate_limit`);
+      let {
+        data: {
+          rate: { remaining },
+        },
+      } = response;
+      setRequests(remaining);
+
+      if (remaining === 0) {
+        // throw an error
+        toggleError(true, 'sorry, hourly rate limit exceeded');
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const searchGithubUser = async (user) => {
+    toggleError();
+    setIsLoading(true);
+    try {
+      const response = await axios(`${rootUrl}/users/${user}`);
+      console.log(response);
+      setGithubUser(response.data);
+
+      if (response) {
+        const { followers_url, repos_url } = response.data;
+        try {
+          const results = await Promise.allSettled([
+            axios(`${followers_url}?per_page=100`),
+            axios(`${repos_url}?per_page=100`),
+          ]);
+
+          const [followers, repos] = results;
+
+          if (followers.status === 'fulfilled') {
+            setFollowers(followers.value.data);
+          }
+
+          if (repos.status === 'fulfilled') {
+            setRepos(repos.value.data);
+          }
+        } catch (error) {
+          console.log(error);
+        }
+      }
+    } catch (error) {
+      console.log(error);
+      toggleError(true, 'user not found');
+    }
+
+    checkRequests();
+    setIsLoading(false);
+  };
+
+  const toggleError = (show = false, msg = '') => {
+    setError({ show, msg });
+  };
+
+  useEffect(() => {
+    checkRequests();
+  }, []);
+
   return (
-    <GithubContext.Provider value={{ githubUser, repos, followers }}>
+    <GithubContext.Provider
+      value={{
+        githubUser,
+        repos,
+        followers,
+        requests,
+        error,
+        searchGithubUser,
+        isLoading,
+      }}
+    >
       {children}
     </GithubContext.Provider>
   );
